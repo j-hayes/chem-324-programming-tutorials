203947e66104b6ef755c23aea8db1af84050cb79
Jackson Hayes <jacksonjhayes@protonmail.com>
2022-03-05 16:52:20 -0600

    one tweak on the linear algebra documentation module 2 first draft done

--- module2/hello-test-runner.jl
+++ module2/hello-test-runner.jl
@@ -1,14 +1,12 @@
 using Test
 include("hello.jl")
 
-function run_tests_module_hello()
-    hello_text = hello()
-    @testset "hello tests" begin;
-         @test hello_text == "hello"
-    end 
+hello_text = hello()
+@testset "hello tests" begin
+     @test hello_text == "hello"
+end
 
-    hello_text = hello("Jackson")
-    @testset "hello name tests" begin;
-        @test hello_text == "hello Jackson"
-   end 
+hello_text = hello("Jackson")
+@testset "hello name tests" begin
+     @test hello_text == "hello Jackson"
 end
